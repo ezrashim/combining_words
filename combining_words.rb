b3c237ef1fc549a473f129a3bda8a1bb312c548f
ezrashim <ezrashim@gmail.com>
2015-11-14 10:06:17 -0500

    initialize

---
+++ combining_words.rb
@@ -0,0 +1,32 @@
+puts "We\'re gonna play a game here."
+puts "Each can only say one word and one word only okay?"
+puts "And we will connect the stories together at the end."
+puts "So, let\'s start!"
+
+#string 1
+puts "Friend 1 says: "
+
+str_1=gets.chomp
+
+#string 2
+puts "Okay, friend 2 says: "
+str_2=gets.chomp
+
+#string 3
+puts "Now, friend 3 says: "
+str_3=gets.chomp
+
+#string 4
+puts "Interesting...friend 4 says: "
+str_4=gets.chomp
+
+#stirng 5
+puts "We\'re not done yet here. Friend 5 says: "
+str_5=gets.chomp
+
+#string 6
+puts "Now, last but not least, friend 6 says: "
+str_6=gets.chomp
+
+puts "Check this out: "
+puts str_1+ " " + str_2 + " " + str_3 + " " +str_4 + " " + str_5 + " " +str_6+ "."
